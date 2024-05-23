82c0f9e26e3564f951023049747599ffebf95fdc
lechatn <noe.lechat@ynov.com>
2024-05-23 14:51:53 +0200

    ajout des boutons

--- static/JS/index.js
+++ static/JS/index.js
@@ -7,3 +7,29 @@ function togglePopup(){
     document.querySelector('#buttonPost').style.display = 'block';
     document.querySelector('.posts').style.display = 'none';
 }
+
+function likedButton(event){
+    console.log("liked");
+    var button = event.target;
+    var otherButton = button.parentElement.querySelector('.dislikeButton');
+    if (button.style.backgroundColor == 'red') {
+        button.style.backgroundColor = 'white';
+        otherButton.disabled = false;
+    } else {
+        button.style.backgroundColor = 'red';
+        otherButton.disabled = true;
+    }  
+}
+
+function dislikedButton(event){
+    console.log("disliked");
+    var button = event.target;
+    var otherButton = button.parentElement.querySelector('.likeButton');
+    if (button.style.backgroundColor == 'blue') {
+        button.style.backgroundColor = 'white';
+        otherButton.disabled = false;
+    } else {
+        button.style.backgroundColor = 'blue';
+        otherButton.disabled = true;
+    }  
+}
